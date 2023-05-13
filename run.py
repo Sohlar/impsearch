d543d281d370ad2ead47bfff91faed9462047cb6
Drew Springfield <drew@robosim.com>
2023-05-13 14:33:05 -0700

    Update run.py

--- run.py
+++ run.py
@@ -4,73 +4,55 @@ import re
 import requests
 from io import BytesIO
 
-""" 
-class PDFScraper:
-    def __init__(self) -> None:
-        pass
+def download_pdf(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return BytesIO(response.content)
+    else:
+        print("Failed to download the PDF. Please check the URL.")
+        return None
 
-    def extractPDF(url):
-        print("Enter URL of PDF")
+def extract_text(pdf_stream):
+    pdf_reader = PyPDF2.PdfReader(pdf_stream)
+    text = ""
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
+    return text
 
-        results = requests.get(url) """
-
-        
-
-class PDFReader:
-    def __init__(self, pdf_path):
-        self.pdf_path = pdf_path
-
-    def extract_text(self) -> str:
-        with open(self.pdf_path, "rb") as file:
-            reader = PyPDF2.PdfFileReader(file)
-            text = ""
-            for page_num in range(reader.numPages):
-                text += reader.getPage(page_num).extractText()
-        return text
-
-    @staticmethod
-    def clean_text(text) -> str:
-        cleaned_text = re.sub(r"[^a-zA-Z0-9.,;:\(\)\[\]\{\}\-\+=\*/ ]", "", text)
-        return cleaned_text
-    
-    def chunk_text(self, text) -> str:
-        section_keywords = [
-            "Introduction",
-            "Background",
-            "Related Work",
-            "Method",
-            "Methodology",
-            "Approach",
-            "Algorithm",
-            "Implementation",
-            "Experiment",
-            "Evaluation",
-            "Results",
-            "Discussion",
-            "Conclusion",
-            "Future Work",
-        ]
-
-        section_pattern = re.compile(
-            r"\b(?:{})\b".format("|".join(re.escape(kw) for kw in section_keywords)),
-            re.IGNORECASE,
-        )
-
-        # Split the text into sections
-        sections = []
-        current_section = []
-
-        for line in text.splitlines():
-            if section_pattern.search(line):
-                if current_section:
-                    sections.append("\n".join(current_section))
-                    current_section = []
-            current_section.append(line)
-
-        if current_section:
-            sections.append("\n".join(current_section))
-
-        return sections
+def chunk_text( text) -> str:
+    section_keywords = [
+        "Introduction",
+        "Background",
+        "Related Work",
+        "Method",
+        "Methodology",
+        "Approach",
+        "Algorithm",
+        "Implementation",
+        "Experiment",
+        "Evaluation",
+        "Results",
+        "Discussion",
+        "Conclusion",
+        "Future Work",
+    ]
+    section_pattern = re.compile(
+        r"\b(?:{})\b".format("|".join(re.escape(kw) for kw in section_keywords)),
+        re.IGNORECASE,
+    )
+    # Split the text into sections
+    sections = []
+    current_section = []
+    for line in text.splitlines():
+        if section_pattern.search(line):
+            if current_section:
+                sections.append("\n".join(current_section))
+                current_section = []
+        current_section.append(line)
+    if current_section:
+        sections.append("\n".join(current_section))
+    return sections
 
 class OpenAIAnalyzer:
     def __init__(self, api_key):
@@ -106,10 +88,7 @@ class OpenAIAnalyzer:
 
 
 
-
-
-
-def main(pdf_path, openai_api_key):
+""" def main(pdf_path, openai_api_key):
     #extract and clean text from PDF
 
     response = requests.get(pdf_path)
@@ -135,8 +114,17 @@ def main(pdf_path, openai_api_key):
     print(summary)
     print("\nPython code snippets:")
     print(code_snippets)
+ """
+def main():
+    url = input("Enter the PDF URL: ")
+    pdf_stream = download_pdf(url)
+    if pdf_stream:
+        text = extract_text(pdf_stream)
+        chunk_text_t = chunk_text(text)
+        print("Extracted text from the PDF:")
+        print(text)
+    else:
+        print("Could not extract text from the PDF.")
 
 if __name__ == "__main__":
-    pdf_path = input("Enter PDF_URL: ")
-    openai_api_key = "your_openai_api_key"
-    main(pdf_path, openai_api_key)
+    main()
